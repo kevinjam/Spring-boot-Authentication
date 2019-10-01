a4430e70fbcec115aae6d438bdc9026be2b01ba5
Kevin Janvier Chinabalire <kevin.chinabalire@cellulant.com>
2019-10-01 14:20:23 +0300

    add password NoOpPasswordEncoder

--- src/main/kotlin/com/kevinjanvier/springsecuritydemo/config/ProjectConfig.kt
+++ src/main/kotlin/com/kevinjanvier/springsecuritydemo/config/ProjectConfig.kt
@@ -1,12 +1,11 @@
 package com.kevinjanvier.springsecuritydemo.config
 
-import com.kevinjanvier.springsecuritydemo.security.AppUserDetailsManager
-import com.kevinjanvier.springsecuritydemo.security.PlainTextPasswordEncoder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
@@ -15,13 +14,14 @@ import org.springframework.security.provisioning.UserDetailsManager
 class ProjectConfig : WebSecurityConfigurerAdapter(){
 
     @Bean
-    fun passwordEncoder():PasswordEncoder = PlainTextPasswordEncoder()
+    fun passwordEncoder():PasswordEncoder= NoOpPasswordEncoder.getInstance()
 
     @Bean
     fun userDetailsManager():UserDetailsManager{
         val manager = InMemoryUserDetailsManager()
         val user = User.withUsername("john")
-                .password("12345").authorities("ADMIN")
+                .password("12345")
+                .authorities("ADMIN")
                 .build()
         manager.createUser(user)
         return manager
